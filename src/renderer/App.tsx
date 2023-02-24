f09f35579162a3e005e994c01e5c52848dd8a411
itsPeetah <pietro.g.moroni@gmail.com>
2023-02-24 12:52:11 +0100

    App

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,54 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+
 import './App.css';
 import 'tailwindcss/tailwind.css';
+import { useState } from 'react';
 
-const Hello = () => {
+function Home() {
+  const [maxScore, setMaxScore] = useState(10);
+
+  function handleMaxScoreChange(valStr: string) {
+    const num = Math.floor(parseFloat(valStr));
+    setMaxScore(num);
+  }
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
+    <div className="w-full h-screen | flex flex-col justify-center items-center">
+      <div className="flex flex-row gap-2 items-center">
+        <label className="text-white" htmlFor="maxScore">
+          Punteggio Massimo:
+        </label>
+        <input
+          name="maxScore"
+          type="number"
+          value={maxScore}
+          min={0}
+          onChange={(v) => handleMaxScoreChange(v.target.value)}
+          className="px-2 py-1 rounded-md"
+        />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+      <div className="grid grid-cols-3 grid-rows-2 place-items-center gap-x-8 | text-center text-white drop-shadow-lg">
+        <h1 className="text-3xl ">Buona</h1>
+        <h1 className="text-5xl">70%</h1>
+        <h1 className="text-3xl ">Cattiva</h1>
+        <h1 className="text-6xl text-lime-500 drop-shadow-lg">
+          {Math.floor(maxScore * 0.7)}
+        </h1>
+        <h1 className="text-8xl drop-shadow-lg">
+          {Math.floor(maxScore * 70) / 100}
+        </h1>
+        <h1 className="text-6xl text-red-500 drop-shadow-lg">
+          {Math.floor(maxScore * 0.7) + 1}
+        </h1>
       </div>
     </div>
   );
-};
+}
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
